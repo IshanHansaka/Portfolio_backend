b627c8a36cfa4cf8d9260046058321630c7c16b3
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-05-28 19:41:08 +0530

    add new p_id columnt to schema

--- Project.js
+++ Project.js
@@ -1,6 +1,7 @@
 const mongoose = require('./db'); //this is one of end point that connect in index.js
 
 const projectSchema = new mongoose.Schema({ //describe about project schema
+    p_id: String,
     name: String,
     description: String,
 });
