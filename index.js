aa9c88080b6c580c28b73fec9715fba9e8c0b045
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-09-29 12:48:36 +0530

    fix: cors issues

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const corsOptions = {
     allowedHeaders: ['Content-Type', 'Authorization'],
   };
   
+app.set('trust proxy', 1);
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 app.use(bodyParser.json());
